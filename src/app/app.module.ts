e2ef75012d9172db0b1830e82630bf3067f50421
AdamzLasu <adam.dobrodziej@akmf.pl>
2023-08-10 20:00:36 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -18,7 +19,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -15,32 +15,35 @@ import { Component } from '@angular/core';
     <div class="container">
 
       <!--Contact form -->
-      <form>
+      <form (ngSubmit)="submitForm()">
         <!-- Name -->
         <div class="field">
           <label class="label">Imię</label>
-          <input type="text" name="name" class="input">
+          <input type="text" name="name" class="input" [(ngModel)]="name">
         </div>
 
         <!-- Surname -->
         <div class="field">
           <label class="label">Nazwisko</label>
-          <input type="text" name="surname" class="input">
+          <input type="text" name="surname" class="input" [(ngModel)]="surname">
         </div>
 
         <!-- Email -->
         <div class="field">
           <label class="label">Email</label>
-          <input type="email" name="email" class="input">
+          <input type="email" name="email" class="input" [(ngModel)]="email">
         </div>
 
         <!-- Message -->
         <div class="field">
-
+          <label class="label">Wiadomość</label>
+          <textarea name="message" class="textarea" [(ngModel)]="message"></textarea>
         </div>
 
         <!-- Button -->
-
+        <button type="submit" class="button is-large is-warning">
+          Wyślij!
+        </button>
 
 
       </form>
@@ -51,6 +54,22 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit  {
+  name: string;
+  surname: string;
+  email: string;
+  message: string;
 
+
+
+  
+  constructor() {}
+
+  ngOnInit() {}
+
+  submitForm(){
+    const message = `My name is ${this.name}. My surname is ${this.surname}. My email is ${this.email}. My message is ${this.message}`;
+    //grab all the fields and their values
+    alert(message)
+  }  
 }
